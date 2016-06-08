19b3852f358265a1cc233bbc87be75523db5887a
Sean Haverty <sean.haverty@gmail.com>
2016-06-08 11:56:04 -0700

    Changed startSql.sh to actually wait for the db to start

--- database/startSql.sh
+++ database/startSql.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 echo "Starting MySQL..."
-docker run --name dbSNP -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 -d mysql:5.7
+docker run --name dbSNP -d \
+	-e MYSQL_DATABASE=alleles \
+	-e MYSQL_ROOT_PASSWORD=password \
+	-p 3306:3306  mysql:5.7
 
 echo "Waiting for db to start..."
-docker exec dbSNP mysqladmin --wait=20 -ppassword ping || exit 1
+sleep 10 
+docker exec dbSNP mysqladmin --silent --wait=20 -ppassword ping || exit 1
 
 echo "Importing sql files..."
+echo "This might take a while..."
 docker exec -i dbSNP mysql -ppassword alleles < Allele.sql
 docker exec -i dbSNP mysql -ppassword alleles < SNPAncestralAllele.sql
