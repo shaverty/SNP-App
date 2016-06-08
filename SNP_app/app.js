a0e913de8ced5dfccd3d6e5d1cdfd8dfc5e2dc58
sean haverty <sean.haverty@gmail.com>
2016-06-08 16:30:51 -0700

    Changed app to return results of database on port 3000, take user input from query

--- SNP_app/app.js
+++ SNP_app/app.js
@@ -3,12 +3,14 @@
 //  Open connection to MySQL server and start it
 
 var mysql      = require('mysql');
+var express    = require('express');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
   database: 'alleles'
 });
+var app = express();
 
 
 connection.connect(function(err){
@@ -18,11 +20,37 @@ connection.connect(function(err){
   }
   console.log('Connection established');
 });
-var snp_id = ''
 
-connection.query('SELECT Allele.allele FROM Allele JOIN SNPAncestralAllele on SNPAncestralAllele.ancestral_allele_id = Allele.allele_id \
- WHERE SNPAncestralAllele.snp_id = ? LIMIT 1', process.argv[2], function(err, data){
-  if(err) throw err;
-  console.log('Data received from DB: ')
-  console.log(data)
-})
+
+function getAncestralAllele(snp_id){
+  return new Promise(function(resolve, reject) {
+    connection.query('SELECT Allele.allele FROM Allele JOIN SNPAncestralAllele on SNPAncestralAllele.ancestral_allele_id = Allele.allele_id \
+     WHERE SNPAncestralAllele.snp_id = ? LIMIT 1', snp_id, function(err, data){
+      if (data.length == 0){
+
+        resolve(undefined);
+      } else {
+        console.log('hhhh');
+        resolve(data);
+      }
+
+    });
+
+  });
+}
+
+app.get("/search",function(req,res){
+  var snp_id = req.query.snp_id;
+
+  getAncestralAllele(snp_id).then(function(allele){
+    if(!allele){
+        res.status(404).send('SNP not found.');
+    } else {
+      res.status(200).send({
+        snp_id: allele
+      });
+    }
+  });
+});
+
+app.listen(3000)
