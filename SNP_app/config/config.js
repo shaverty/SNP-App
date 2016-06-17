94f00df6d3aa8b97fa83e4f81244d9d17980efeb
sean haverty <sean.haverty@gmail.com>
2016-06-17 00:37:18 -0700

    forgot to add files

---
+++ SNP_app/config/config.js
@@ -0,0 +1,11 @@
+//  config.js
+//
+//  Application configuration file
+
+module.exports = {
+  host: 'localhost',
+  user: 'root',
+  passowrd: 'password',
+  database: 'alleles',
+  port: 3306
+};
